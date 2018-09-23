77f6cb0e47941316ea19d77c355e3beebf49e2ad
Lorenzo Kniss <lorenzo.kniss@acad.pucrs.br>
2018-09-23 10:55:57 -0300

    Adicionando arquivo do loader.io para testes em produção

--- routes/app-route.js
+++ routes/app-route.js
@@ -3,5 +3,8 @@ var router = express.Router();
 var appController = require('../controllers/app-controller');
 
 router.get('/', appController.index);
+router.get('/loaderio-c206be629e08a69f527bfb7c162f1b14', function (req, res) {
+    res.sendFile('/files/verification_file.txt', {root: './public'});
+});
 
 module.exports = router;
