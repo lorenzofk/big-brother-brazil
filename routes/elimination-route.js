cd269b0b399babaf9344bd547813a2a4c4863315
Lorenzo Kniss <lorenzo.kniss@acad.pucrs.br>
2018-09-23 21:58:41 -0300

    Comentando rotas

--- routes/elimination-route.js
+++ routes/elimination-route.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var eliminationController = require('../controllers/elimination-controller');
 
+// Routes used like as RESTful API
+
 router.get('/', eliminationController.list);
 router.get('/:id', eliminationController.show);
 router.delete('/:id', eliminationController.delete);
